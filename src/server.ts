df58d06485f894e74cd50d540b8273579ebb8995
felipe086 <felipegalegoosawa@gmail.com>
2024-05-23 22:08:37 -0300

    Revert "feat: add create event route"
    This reverts commit 098967433504e938a6f6744a2ce79befdef64681.

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,10 @@
 import fastify from "fastify"
-import { z } from "zod"
-import { prisma } from "./lib/prisma"
-import { generateSlug } from "./utils/generate-slug"
-const PORT = 3333
 
 const app = fastify()
+const PORT = 3333
 
-app.post("/events", async (req, reply) => {
-  const requestBodySchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-  const { title, details, maximumAttendees } = requestBodySchema.parse(req.body)
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      slug: generateSlug(title),
-      maximumAttendees,
-    },
-  })
-
-  return reply.status(201).send({ eventId: event.id })
+app.get("/", (req, reply) => {
+  return reply.send({ message: "Hi" })
 })
 
 app

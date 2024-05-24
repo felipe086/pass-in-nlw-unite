d01db322e77fb38ac0b1818cb40af313d79d3c6d
felipe086 <felipegalegoosawa@gmail.com>
2024-05-23 23:17:53 -0300

    feat: add register for event route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod"
 import { createEvent } from "./routes/create-event"
+import { registerForEvent } from "./routes/register-for-event"
 const PORT = 3333
 
 const app = fastify()
@@ -12,6 +13,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app
   .listen({ port: PORT })
